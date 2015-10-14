e99156c0a0841a63e03b111b26d7973ed9e959a7
root <reg.lo@yandex.ru>
2015-10-14 16:27:50 +0300

    Фиксы в оформлении

--- public_html/js/views/logout.js
+++ public_html/js/views/logout.js
@@ -14,16 +14,15 @@ define([
         template: tmpl,
         model: new logout(),
         events: {
-            "click .button_logout" : "onSubmit",
-            "click .logout__on-main-page" : "onMain"
+            "click .button_logout" : "onSubmit"
         },
         initialize: function () {
             console.log("logout initialize");
         },
         render: function () {
             console.log("logout render");
-            var data = {"name": auth_user.name, "score": auth_user.score};
-            this.$el.html(this.template(data));
+            //var data = {"name": auth_user.name, "score": auth_user.score};
+            //this.$el.html(this.template(data));
         },
         show: function () {
             this.$el.show();
@@ -32,18 +31,10 @@ define([
             this.$el.hide();
 
         },
-
         onSubmit: function() {
             console.log("logout-view logout click event");
-            this.model.trigger("submit")
-        },
-        onMain: function() {
-            location.href = $(".logout__on-main-page").attr('data-href');
+            this.model.onSubmit();
         }
-        //onLogout: function() {
-        //    console.log("logout-view success logout");
-        //    this.user.trigger("successLogout",{"param" : "myParam"});
-        //}
     });
 
     return new View();
