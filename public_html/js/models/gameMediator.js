0c8671957bad3a83a42ecc7697e7c8ef56dfcb80
ivan <pohodnyaivan@gmail.com>
2015-11-05 19:25:49 +0300

    ws::/g14 to localhost

--- public_html/js/models/gameMediator.js
+++ public_html/js/models/gameMediator.js
@@ -20,7 +20,7 @@ define (['backbone'], function(Backbone) {
             this.get('field').on('exit', this.exit.bind(this));
         },
         initializeSocket: function() {
-            var socket = new WebSocket("ws://g14.javaprojects.tp-dev.ru/gameplay");//todo localhost в константы
+            var socket = new WebSocket("ws://localhost:8000/gameplay");
             socket.onopen = function(event) {
                 console.log("____ open socket");
                 this.beginningGameWaiting();
